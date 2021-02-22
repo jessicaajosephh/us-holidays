c9fdff1f69cc16f59d8d7788f9ae53d4d09424d2
Jessica Joseph <jessicaajosephh@gmail.com>
2021-02-22 18:20:28 -0500

    created call, greeting and menu method inside cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,19 @@
 class CLI
     
+    def call
+        puts greeting
+        API.get_holiday_data
+        menu
+    end
+
+    def greeting
+        puts "Welcome! Start searching for holidays now!"
+        puts "To search for US federal holidays, enter 'holidays'"
+        puts "If there is nothing else you would like to do at the moment, enter 'exit'"
+    end
+
+    def menu 
+        
+    end
 
 end
